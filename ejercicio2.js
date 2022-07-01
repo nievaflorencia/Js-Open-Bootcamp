310062a66f270d6b6c6894d7279b9ba9c9e7edf6
Florencia <nievaflorenciadv@gmail.com>
2022-07-01 16:13:20 -0300

    ejercicio2

---
+++ ejercicio2.js
@@ -0,0 +1,19 @@
+//- Tu nombre (string);
+const name = ("Flor");
+//- Tu edad (number);
+const age = 32;
+//- Eres desarrollador (boolean);
+let dev = true;
+
+
+//- Tu fecha de nacimiento (Date)
+
+const birthday = new Date ("december 14 1989");
+
+//- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const book = {
+    título: "Harry Potter",
+    author: "Jk Rowling",
+    date: 1997
+}
